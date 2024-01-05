fdc1096aba11690c938048b84c32d7e680b17943
Kenta Kato <kato@robotics.tokyo>
2024-01-05 23:40:31 +0900

    estimate mean and variance

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,7 @@
 use rand::Rng;
 use rand_distr::{Normal, Distribution};
-use ndarray;
 use ndarray::{Array1, Array2, ArrayView1};
-use ndarray_linalg::solve::Inverse;
+use ndarray_linalg::Solve;
 use std::f64::consts::PI;
 use plotters::{prelude::*, coord::types::RangedCoordf64};
 
@@ -13,9 +12,9 @@ fn sin_func(x: f64) -> f64 {
     return amp * f64::sin(omega * x + phase);
 }
 
-fn draw_latent_func(
+fn draw_func(
     chart: &mut ChartContext<BitMapBackend, Cartesian2d<RangedCoordf64, RangedCoordf64>>,
-    latent_f: fn(f64) -> f64,
+    func: fn(f64) -> f64,
     bounds: (f64, f64)) -> Result<(), Box<dyn std::error::Error>> {
 
     let step = 0.1;
@@ -23,17 +22,59 @@ fn draw_latent_func(
         .map(|i| bounds.0 + i as f64 * step)
         .filter(|&x| x <= bounds.1);
     chart.draw_series(LineSeries::new(
-        range.map(|x| (x, latent_f(x))),
+        range.map(|x| (x, func(x))),
         &BLUE
     ))?;
 
     return Ok(());
 }
 
+fn draw_points(
+    chart: &mut ChartContext<BitMapBackend, Cartesian2d<RangedCoordf64, RangedCoordf64>>,
+    x_array: ArrayView1<f64>,
+    y_array: ArrayView1<f64>,
+    color: RGBColor,
+    point_size: i32,
+    enable_line: bool) -> Result<(), Box<dyn std::error::Error>> {
+
+    for (x, y) in x_array.iter().zip(y_array.iter()) {
+        chart.draw_series(PointSeries::of_element(
+            vec![(*x, *y)], // ベクトル (x, y) を作成
+            point_size, // 点のサイズ
+            ShapeStyle::from(&color).filled(), // 点のスタイル
+            &|coord, size, style| { // 無名関数（クロージャ）を作成。&で参照キャプチャを指定。
+                EmptyElement::at(coord) // 点の座標を指定（空の要素を配置）
+                    + Circle::new((0, 0), size, style) // 円を描画
+            },
+        ))?;
+    }
+
+    if enable_line {
+        chart.draw_series(LineSeries::new(
+            x_array.iter().zip(y_array.iter()).map(|(&x, &y)| (x, y)),
+            &color,
+        ))?;
+    }
+    return Ok(());
+}
+
+fn draw_line(
+    chart: &mut ChartContext<BitMapBackend, Cartesian2d<RangedCoordf64, RangedCoordf64>>,
+    x_array: ArrayView1<f64>,
+    y_array: ArrayView1<f64>,
+    color: RGBColor) -> Result<(), Box<dyn std::error::Error>> {
+
+    chart.draw_series(LineSeries::new(
+        x_array.iter().zip(y_array.iter()).map(|(&x, &y)| (x, y)),
+        &color
+    ))?;
+    return Ok(());
+}
+
 // Radial Basis Function
 fn rbf_kernel(x1: f64, x2: f64) -> f64 {
-    let theta1 = 1.0;
-    let theta2 = 1.0;
+    let theta1 = 0.1;
+    let theta2 = 0.1;
     return theta1 * f64::exp(-(x1 - x2).powi(2) / theta2);
 }
 
@@ -68,22 +109,39 @@ fn compute_k_star(
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut rng = rand::thread_rng(); // 乱数ジェネレータを初期化
 
-    let num_train = 10;
+    const NUM_TRAIN: i32 = 15;
     let normal_dist = Normal::new(0.0, 0.1).unwrap();
-    let x_train: Array1::<f64> = (0..num_train)
+    let x_train: Array1::<f64> = (0..NUM_TRAIN)
         .map(|_| {
             return rng.gen_range(-PI..PI);
         }).collect();
     let y_train = x_train.map(|&x| sin_func(x) + normal_dist.sample(&mut rng));
 
-    let x_star = ndarray::array![-1.0, 0.0, 1.5, 2.1];
+    let x_star = ndarray::Array::linspace(-PI, PI, 100);
     let gram = compute_gram_matrix(&x_train, rbf_kernel);
     let k_star = compute_k_star(x_train.view(), x_star.view(), rbf_kernel);
 
-    // --- compute transpose(k_star) * inv(gram) * y_train ---
-    let gram_inv = Inverse::inv(&gram);
+    // --- compute mean: transpose(k_star) * inv(gram) * y_train ---
+    let gram_inv_y = gram.solve(&y_train).unwrap();
     let k_star_t = k_star.t();
-    let result = k_star_t.dot(&gram_inv).dot(&y_train);
+    let y_star = k_star_t.dot(&gram_inv_y);
+
+    // --- compute variance: diag(k_star_t * inv(gram) * k_star) ---
+    let mut gram_inv = Array2::<f64>::zeros((x_train.len(), x_train.len()));
+    for i in 0..x_train.len() {
+        let mut unit_vector = Array1::<f64>::zeros(x_train.len());
+        unit_vector[i] = 1.0;
+        let col = gram.solve(&unit_vector).unwrap();
+        gram_inv.column_mut(i).assign(&col);
+    }
+
+    let variance_matrix = k_star_t.dot(&gram_inv).dot(&k_star);
+    let y_star_variance = variance_matrix.diag().to_owned();
+    let upper = &y_star + &y_star_variance.mapv(f64::sqrt);
+    let lower = &y_star - &y_star_variance.mapv(f64::sqrt);
+
+
+
 
     // グラフを描画する画像ファイルを作成
     let root = BitMapBackend::new("images/plot.png", (640, 480)).into_drawing_area();
@@ -102,21 +160,13 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     // XY軸、グリッド線を描画
     chart.configure_mesh().draw()?;
 
-    for (x, y) in x_train.iter().zip(y_train.iter()) {
-        chart.draw_series(PointSeries::of_element(
-            vec![(*x, *y)], // ベクトル (x, y) を作成
-            5, // 点のサイズ
-            ShapeStyle::from(&RED).filled(), // 点のスタイル
-            &|coord, size, style| { // 無名関数（クロージャ）を作成。&で参照キャプチャを指定。
-                EmptyElement::at(coord) // 点の座標を指定（空の要素を配置）
-                    + Circle::new((0, 0), size, style) // 円を描画
-                    + Text::new(format!("{:.2}", coord.1), (10, 0), ("sans-serif", 15).into_font()) // y の値を表示
-            },
-        ))?;
-    }
+    draw_points(&mut chart, x_train.view(), y_train.view(), MAGENTA, 5, false)?;
+    draw_line(&mut chart, x_star.view(), y_star.view(), GREEN)?;
+    draw_line(&mut chart, x_star.view(), upper.view(), CYAN)?;
+    draw_line(&mut chart, x_star.view(), lower.view(), CYAN)?;
 
     // sin関数を描画
-    draw_latent_func(&mut chart, sin_func, (-PI, PI))?;
+    draw_func(&mut chart, sin_func, (-PI, PI))?;
 
     // 描画が終わったら、画像ファイルを保存
     root.present()?;
