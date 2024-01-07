723f92e6d974d7bf704b64b99ceffdf4d779fa21
Kenta Kato <kato@robotics.tokyo>
2024-01-07 16:36:03 +0900

    plot関連をplot_utilsに分離

--- src/main.rs
+++ src/main.rs
@@ -1,75 +1,17 @@
 mod gausiaan_process;
+mod plot_utils;
 
 use rand::Rng;
 use rand_distr::{Normal, Distribution};
-use ndarray::{Array1, ArrayView1};
+use ndarray::Array1;
 use std::f64::consts::PI;
-use plotters::{prelude::*, coord::types::RangedCoordf64};
+use plotters::prelude::*;
 
 fn sin_func(x: f64) -> f64 {
     let amp = 1.0;
     let omega = 1.0;
     let phase = 0.0;
     return amp * f64::sin(omega * x + phase);
-}
-
-fn draw_func(
-    chart: &mut ChartContext<BitMapBackend, Cartesian2d<RangedCoordf64, RangedCoordf64>>,
-    func: fn(f64) -> f64,
-    bounds: (f64, f64)) -> Result<(), Box<dyn std::error::Error>> {
-
-    let step = 0.1;
-    let range = (0..=((bounds.1 - bounds.0) / step).round() as usize)
-        .map(|i| bounds.0 + i as f64 * step)
-        .filter(|&x| x <= bounds.1);
-    chart.draw_series(LineSeries::new(
-        range.map(|x| (x, func(x))),
-        &BLUE
-    ))?;
-
-    return Ok(());
-}
-
-fn draw_points(
-    chart: &mut ChartContext<BitMapBackend, Cartesian2d<RangedCoordf64, RangedCoordf64>>,
-    x_array: ArrayView1<f64>,
-    y_array: ArrayView1<f64>,
-    color: RGBColor,
-    point_size: i32,
-    enable_line: bool) -> Result<(), Box<dyn std::error::Error>> {
-
-    for (x, y) in x_array.iter().zip(y_array.iter()) {
-        chart.draw_series(PointSeries::of_element(
-            vec![(*x, *y)], // ベクトル (x, y) を作成
-            point_size, // 点のサイズ
-            ShapeStyle::from(&color).filled(), // 点のスタイル
-            &|coord, size, style| { // 無名関数（クロージャ）を作成。&で参照キャプチャを指定。
-                EmptyElement::at(coord) // 点の座標を指定（空の要素を配置）
-                    + Circle::new((0, 0), size, style) // 円を描画
-            },
-        ))?;
-    }
-
-    if enable_line {
-        chart.draw_series(LineSeries::new(
-            x_array.iter().zip(y_array.iter()).map(|(&x, &y)| (x, y)),
-            &color,
-        ))?;
-    }
-    return Ok(());
-}
-
-fn draw_line(
-    chart: &mut ChartContext<BitMapBackend, Cartesian2d<RangedCoordf64, RangedCoordf64>>,
-    x_array: ArrayView1<f64>,
-    y_array: ArrayView1<f64>,
-    color: RGBColor) -> Result<(), Box<dyn std::error::Error>> {
-
-    chart.draw_series(LineSeries::new(
-        x_array.iter().zip(y_array.iter()).map(|(&x, &y)| (x, y)),
-        &color
-    ))?;
-    return Ok(());
 }
 
 // Radial Basis Function
@@ -115,13 +57,13 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     // XY軸、グリッド線を描画
     chart.configure_mesh().draw()?;
 
-    draw_points(&mut chart, x_train.view(), y_train.view(), MAGENTA, 5, false)?;
-    draw_line(&mut chart, x_star.view(), mean.view(), GREEN)?;
-    draw_line(&mut chart, x_star.view(), upper.view(), CYAN)?;
-    draw_line(&mut chart, x_star.view(), lower.view(), CYAN)?;
+    plot_utils::draw_points(&mut chart, x_train.view(), y_train.view(), MAGENTA, 5, false)?;
+    plot_utils::draw_line(&mut chart, x_star.view(), mean.view(), GREEN)?;
+    plot_utils::draw_line(&mut chart, x_star.view(), upper.view(), CYAN)?;
+    plot_utils::draw_line(&mut chart, x_star.view(), lower.view(), CYAN)?;
 
     // sin関数を描画
-    draw_func(&mut chart, sin_func, (-PI, PI))?;
+    plot_utils::draw_func(&mut chart, sin_func, (-PI, PI))?;
 
     // 描画が終わったら、画像ファイルを保存
     root.present()?;
---
+++ src/plot_utils.rs
@@ -0,0 +1,61 @@
+use plotters::{prelude::*, coord::types::RangedCoordf64};
+use ndarray::ArrayView1;
+
+pub fn draw_func(
+    chart: &mut ChartContext<BitMapBackend, Cartesian2d<RangedCoordf64, RangedCoordf64>>,
+    func: fn(f64) -> f64,
+    bounds: (f64, f64)) -> Result<(), Box<dyn std::error::Error>> {
+
+    let step = 0.1;
+    let range = (0..=((bounds.1 - bounds.0) / step).round() as usize)
+        .map(|i| bounds.0 + i as f64 * step)
+        .filter(|&x| x <= bounds.1);
+    chart.draw_series(LineSeries::new(
+        range.map(|x| (x, func(x))),
+        &BLUE
+    ))?;
+
+    return Ok(());
+}
+
+pub fn draw_points(
+    chart: &mut ChartContext<BitMapBackend, Cartesian2d<RangedCoordf64, RangedCoordf64>>,
+    x_array: ArrayView1<f64>,
+    y_array: ArrayView1<f64>,
+    color: RGBColor,
+    point_size: i32,
+    enable_line: bool) -> Result<(), Box<dyn std::error::Error>> {
+
+    for (x, y) in x_array.iter().zip(y_array.iter()) {
+        chart.draw_series(PointSeries::of_element(
+            vec![(*x, *y)], // ベクトル (x, y) を作成
+            point_size, // 点のサイズ
+            ShapeStyle::from(&color).filled(), // 点のスタイル
+            &|coord, size, style| { // 無名関数（クロージャ）を作成。&で参照キャプチャを指定。
+                EmptyElement::at(coord) // 点の座標を指定（空の要素を配置）
+                    + Circle::new((0, 0), size, style) // 円を描画
+            },
+        ))?;
+    }
+
+    if enable_line {
+        chart.draw_series(LineSeries::new(
+            x_array.iter().zip(y_array.iter()).map(|(&x, &y)| (x, y)),
+            &color,
+        ))?;
+    }
+    return Ok(());
+}
+
+pub fn draw_line(
+    chart: &mut ChartContext<BitMapBackend, Cartesian2d<RangedCoordf64, RangedCoordf64>>,
+    x_array: ArrayView1<f64>,
+    y_array: ArrayView1<f64>,
+    color: RGBColor) -> Result<(), Box<dyn std::error::Error>> {
+
+    chart.draw_series(LineSeries::new(
+        x_array.iter().zip(y_array.iter()).map(|(&x, &y)| (x, y)),
+        &color
+    ))?;
+    return Ok(());
+}
